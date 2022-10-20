996cdb80dba91e0b229295f6550b8dd0c1698a93
maxime.bienassis <maxime.bienassis@octo.com>
2022-10-20 08:14:15 +0200

    feat(poiscaille-équilibrage): Ajout étape 2 et 3 de l'équilibrage

--- src/equilibrage.js
+++ src/equilibrage.js
@@ -1,6 +1,34 @@
 const NOMBRE_DE_JOURS_ENTRE_DEUX_COMMANDES = 7;
 
 
+function ventilerResteDesCasiers( commandeTropGrande,commandeSuivante, resteDesCasiers, pointRelais) {
+
+        for (let i = 0; i < resteDesCasiers  ; i++) {
+            commandeTropGrande.places[pointRelais[i]]--;
+            commandeSuivante.places[pointRelais[i]]++;
+
+    }
+}
+
+function equilibrerPointRelais(differenceAReporter, commandeTropGrande, commandeSuivante) {
+    const pointRelais = Object.keys(commandeTropGrande.places);
+    const nombreDePointsRelais = pointRelais.length;
+    let nombreDeCasierDéplacé = 0;
+
+    for (let i = 0; i < nombreDePointsRelais; i++) {
+        const nombreDeCasierADeplacer = Math.floor(differenceAReporter / commandeTropGrande.lockers * commandeTropGrande.places[pointRelais[i]]);
+        commandeTropGrande.places[pointRelais[i]] -= nombreDeCasierADeplacer;
+        commandeSuivante.places[pointRelais[i]] += nombreDeCasierADeplacer;
+        nombreDeCasierDéplacé += nombreDeCasierADeplacer;
+    }
+    const resteDesCasiers = differenceAReporter - nombreDeCasierDéplacé;
+
+    if (resteDesCasiers > 0) {
+        ventilerResteDesCasiers(commandeTropGrande, commandeSuivante, resteDesCasiers, pointRelais);
+    }
+
+}
+
 function trouverEquilibre(listeCommande) {
 
     const moyenneCasier = getMoyenneCasier(listeCommande);
@@ -10,23 +38,7 @@ function trouverEquilibre(listeCommande) {
     listeCommandeTropGrande.map(commandeTropGrande => {
         const commandeSuivante = listeCommande.find(commande => getCommandeSuivante(commande, commandeTropGrande));
         const differenceAReporter = calculerDifferenceAReporter(commandeTropGrande, commandeSuivante, moyenneCasier);
-
-
-
-        const nombreDeCasierADeplacer = differenceAReporter / commandeTropGrande.lockers * commandeTropGrande.places.a;
-
-        commandeTropGrande.places.a -= nombreDeCasierADeplacer;
-        commandeSuivante.places.a += nombreDeCasierADeplacer;
-
-        const nombreDeCasierADeplacerB = differenceAReporter / commandeTropGrande.lockers * commandeTropGrande.places.b;
-
-        commandeTropGrande.places.b -= nombreDeCasierADeplacerB;
-        commandeSuivante.places.b += nombreDeCasierADeplacerB;
-
-        const nombreDeCasierADeplacerC = differenceAReporter / commandeTropGrande.lockers * commandeTropGrande.places.c;
-
-        commandeTropGrande.places.c -= nombreDeCasierADeplacerC;
-        commandeSuivante.places.c += nombreDeCasierADeplacerC;
+        equilibrerPointRelais(differenceAReporter, commandeTropGrande, commandeSuivante);
 
         commandeTropGrande.lockers -= differenceAReporter;
         commandeSuivante.lockers += differenceAReporter;
--- src/equilibrage.spec.js
+++ src/equilibrage.spec.js
@@ -137,6 +137,90 @@ describe('équilibrage', () => {
             });
 
         });
+        describe('Si une semaine possède chacun 6 points relais', () => {
+            it('doit faire passer 3 fois plus de casiers à la semaine suivante ', () => {
+                // Given
+                const premierCasier = {
+                    date: 20220404,
+                    lockers: 120,
+                    places: {a: 70, b: 10, c: 10, d: 10, e: 10, f: 10}
+                };
+
+                const deuxièmeCasier = {
+                    date: 20220411,
+                    lockers: 60,
+                    places: {a: 10, b: 10, c: 10, d: 10, e: 10, f: 10}
+                };
+
+                const listeCasier = [premierCasier, deuxièmeCasier];
+
+                // When
+                const listeCasierTrié = trouverEquilibre(listeCasier);
+                const premierCasierTrié = listeCasierTrié[0];
+                const deuxièmeCasierTrié = listeCasierTrié[1];
+
+                // Then
+                expect(premierCasierTrié.places.a).toEqual(52);
+                expect(premierCasierTrié.places.b).toEqual(7);
+                expect(premierCasierTrié.places.c).toEqual(7);
+                expect(premierCasierTrié.places.d).toEqual(8);
+                expect(deuxièmeCasierTrié.places.a).toEqual(28);
+                expect(deuxièmeCasierTrié.places.b).toEqual(13);
+                expect(deuxièmeCasierTrié.places.c).toEqual(13);
+                expect(deuxièmeCasierTrié.places.d).toEqual(12);
+
+            });
+
+        });
+
+        describe("Si tout les casiers n'ont pas été passé" , () => {
+            it('doit renvoyer les suivants 1 par 1', () => {
+                // Given
+                const casierNonEquilibrés = [{
+                    date: 20220404,
+                    lockers: 45,
+                    places: {a: 20, b: 15, c: 10}
+                }, {
+                    date: 20220411,
+                    lockers: 29,
+                    places: {a: 10, b: 10, c: 9}
+                }, {
+                    date: 20220418,
+                    lockers: 70,
+                    places: {a: 30, b: 20, c: 20}
+                }, {
+                    date: 20220425,
+                    lockers: 20,
+                    places: {a: 5, b: 10, c: 5}
+                }];
+
+                const casierAttendu = [{
+                    date: 20220404,
+                    lockers: 41,
+                    places: {a: 18, b: 13, c: 10}
+                }, {
+                    date: 20220411,
+                    lockers: 33,
+                    places: {a: 12, b: 12, c: 9}
+                }, {
+                    date: 20220418,
+                    lockers: 49,
+                    places: {a: 21, b: 14, c: 14}
+                }, {
+                    date: 20220425,
+                    lockers: 41,
+                    places: {a: 14, b: 16, c: 11}
+                }];
+
+                // When
+                const casierEquilibré = trouverEquilibre(casierNonEquilibrés)
+
+                // Then
+                expect(casierEquilibré).toEqual(casierAttendu);
+
+
+            });
+        });
         xdescribe('quand on envoie 4 casiers non équilibré en entrée ', () => {
             it('doit retourner 4 casiers équilibrés ', () => {
                 // Given
